b49d70a284000fed1a0091a076c7cb3982a50b91
ShubhamChaudharyy <43263087+ShubhamChaudharyy@users.noreply.github.com>
2019-07-10 22:27:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
    
 	var x = document.getElementById("userdetail1");
+	var x1=document.getElementById("userdetail1");
 	var y = document.getElementById("userdetail2");
+	var y1=document.getElementById("userdetail2");
     var verifynam=document.getElementById("exitdetail1");
+    var vn1=document.getElementById("exitdetail1");
 	
 	var verifyspot = document.getElementById("exitdetail2");
+	var vs=document.getElementById("exitdetail2");
 	var submit=document.getElementById("submitlis");
 	var changa=document.querySelectorAll("td");
 	var g = document.getElementById("update");
     var remove=document.getElementById("removelis");    
 	var check = new Array();
+	var c;
+	var g;
+	var c1;
 	
-	
-
+	var c11;
+	var e ;
+	var e1;
+   
 
 	for( var i=0;i<changa.length;i++)
 	{ 
@@ -20,6 +29,7 @@
 	    
 	    
 	};
+	
    
    var verifyname=new Array();
 
@@ -30,9 +40,13 @@
    
    submit.addEventListener("click",function(){
   for(var i=0;i<changa.length;i++)
-  {
-  	if(check[i]==y.value)
+  {  
+    c1=y.value/1000; 
+    c11=parseInt(c1);
+      
+  	if(check[i]==c11)
   	{    console.log(i);
+  		check[i]=y.value;
   		
   		changa[i].textContent="\xa0\xa0\xa0\xa0"+"RESERVED"+"\n"+"\xa0\xa0\xa0\xa0"+ x.value;
   		changa[i].style.textTransform="capitalize";
@@ -47,26 +61,37 @@
   	    changa[i].style.backgroundColor="#A12914";
         g.textContent=--p1score;
         verifyname[i]=x.value;
+         $(x1).val("");
+         $(y1).val("");
+         console.log(y.value(word.length+1).join(“*”));
+        
   	}
   }
    });
    remove.addEventListener("click",function(){
   for(var i=0;i<changa.length;i++)
-  {
+  { 
+  	
   	if((check[i]==verifyspot.value)&&(verifyname[i]==verifynam.value))
 
   	{ 
 
   	     console.log(i);
+  	     e=verifyspot.value/1000;
+        e1=parseInt(e);
+        check[i]=e1;
   		changa[i].textContent="\xa0\xa0\xa0\xa0"+"AVAILABLE";
         
   	
   		changa[i].style.color="#282D27";
-  		changa[i].style.font=" bold 40px Passion One";
+  		changa[i].style.font="bold 40px Passion One";
   		changa[i].style.objectFit="cover";
   		
   		changa[i].style.backgroundColor="#569022";
         g.textContent=++p1score;
+        $(vn1).val("");
+        $(vs).val("");
+        
   	}
    
   }
